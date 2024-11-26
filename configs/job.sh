3dcfa5afb387526b66a82c7f02783864fb198487
Damilare Oluwole <doluwole@parkway.ng>
2024-11-26 10:18:11 +0100

    update job.sh for worker

--- configs/job.sh
+++ configs/job.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo "[ cronjob task ] executed at `date`" > /proc/1/fd/2
+
+# Log the execution time
+echo "[Laravel Scheduler] Task executed at $(date)" >> /var/www/html/storage/logs/laravel-scheduler.log
+
+# Run the Laravel scheduler using absolute path
+php /var/www/html/artisan schedule:run >> /var/www/html/storage/logs/laravel-scheduler.log 2>&1
+
+
