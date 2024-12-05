143c6bab729cf9c265d332f56a55820eb043e546
Damilare Oluwole <doluwole@parkway.ng>
2024-12-05 10:43:48 +0100

    fix

--- configs/job.sh
+++ configs/job.sh
@@ -1,9 +1,1 @@
 #!/bin/bash
-
-# Log the execution time
-echo "[Laravel Scheduler] Task executed at $(date)" >> /var/www/html/storage/logs/laravel-scheduler.log
-
-# Run the Laravel scheduler using absolute path
-php /var/www/html/artisan schedule:run >> /var/www/html/storage/logs/laravel-scheduler.log 2>&1
-
-
